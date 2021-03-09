20d0a5492712e304f17e33260e027a20d53f0b3e
hritikchokker <hchokker7@gmail.com>
2021-03-09 18:48:00 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, Injector, VERSION } from "@angular/core";
+import { createCustomElement } from "@angular/elements";
+import { PopupComponent } from "./popup/popup.component";
+import { PopupService } from "./popup/popup.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  constructor(injector: Injector, public popup: PopupService) {
+    // Convert `PopupComponent` to a custom element.
+    const PopupElement = createCustomElement(PopupComponent, { injector });
+    // Register the custom element with the browser.
+    customElements.define("popup-element", PopupElement);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
-import { PopupComponent } from './popup/popup.component';
+import { PopupComponent } from "./popup/popup.component";
+import { PopupService } from "./popup/popup.service";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   declarations: [AppComponent, HelloComponent, PopupComponent],
   bootstrap: [AppComponent],
-  entryComponents: [PopupComponent]
+  entryComponents: [PopupComponent],
+  providers: [PopupService]
 })
 export class AppModule {}
